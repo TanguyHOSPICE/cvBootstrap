9724c0a425adadedd25ff0ad4fb43af6e672f25a
tanguy hospice <hospicetanguy@gmail.com>
2021-04-22 16:30:30 -0400

    Add lib typed for presentation txt

---
+++ script.js
@@ -0,0 +1,9 @@
+//************************************** Typed
+
+var typed = new Typed('.typed', {
+    strings: ["Je suis devenu récemment dévelloppeur full stack suite à une reconversion total (venant du transport maritime).Après un apprentissage toujours remis en question, j'ai poursuivit ma formation par correspondance"],
+    typeSpeed: 10,
+    backSpeed: 0,
+    smartBackspace: false, // this is a default
+    loop: true
+  });
